513b9696f9b1a2b46dc88720959179398d536a79
ed <edimoldovan@gmail.com>
2022-09-03 19:43:39 +0200

    build css with parcel-css
    https://github.com/parcel-bundler/parcel-css

--- build.sh
+++ build.sh
@@ -8,17 +8,14 @@
 echo 'copying javascript files to public folder...';
 cp assets/node_modules/flatpickr/dist/flatpickr.min.js public/js;
 
-echo 'building global css with postcss...'
-cd assets/css-toolchain/; npx postcss src/css/global.css -o dist/css/global.css; cd ..; cd ..;
+echo 'building and minifying global css with rust-based postcss...'
+cd assets/css-toolchain/; npx parcel-css --bundle -m --nesting src/css/global.css -o ../../public/global.min.css; cd ..; cd ..;
 
-echo 'minifying global css...';
-minify assets/css-toolchain/dist/css/global.css -o public/global.min.css;
+# echo 'minifying style css...';
+# minify assets/style.css -o public/style.min.css;
 
-echo 'minifying style css...';
-minify assets/style.css -o public/style.min.css;
-
-echo 'minifying blog css...';
-minify assets/blog.css -o public/blog.min.css;
+# echo 'minifying blog css...';
+# minify assets/blog.css -o public/blog.min.css;
 
 # /usr/local/go/bin/go build -o ./tmp/main .
 go build -o ./tmp/main .
