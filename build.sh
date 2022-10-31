f1ef6296beb912cfa42d30e4bd0c4eb4de6bd669
ed <edimoldovan@gmail.com>
2022-10-31 18:59:26 +0100

    islands hydration demo

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 
 echo 'copying javascript files to public folder...';
 cp assets/node_modules/flatpickr/dist/flatpickr.min.js public/js;
+cp assets/js/web-component.js public/js/;
+cp assets/js/is-land.js public/js/;
 
 echo 'building and minifying global css with rust-based postcss...'
-cd assets/css/; npx lightningcss --bundle -m --nesting global.css -o ../../public/global.min.css; cd ..; cd ..;
+cd assets/css/; npx lightningcss --bundle -m --nesting global.css -o ../../public/css/global.min.css; cd ..; cd ..;
 
 echo 'copying fonts...'
 cp assets/fonts/* public/fonts/
